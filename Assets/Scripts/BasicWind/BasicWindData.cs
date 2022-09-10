b4b5765003d8b057afeae4778b423fb1c86a334d
Thomas ICHÉ <4037271+peeweek@users.noreply.github.com>
2022-09-10 20:20:06 +0200

    Update

--- Assets/Scripts/BasicWind/BasicWindData.cs
+++ Assets/Scripts/BasicWind/BasicWindData.cs
@@ -34,7 +34,13 @@ namespace HDRPSamples
 
         void ApplySettings()
         {
+            if (Camera.main == null)
+            {
+                return;
+            }
+
             var stack = HDCamera.GetOrCreate(Camera.main).volumeStack;
+
             var windSettings = stack.GetComponent<BasicWind>();
 
             if (windSettings == null)
